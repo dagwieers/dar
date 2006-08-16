df882ddd1449d4f4d69a3aa4d3c10744b0f86d62
Dag Wieers <dag@wieers.com>
2006-08-16 15:10:41 +0000

    Small improvements

--- scripts/pre-new-release.sh
+++ scripts/pre-new-release.sh
@@ -3,7 +3,7 @@
 ### This script intervenes when a release tag conforms to the new scheme (has .2)
 
 script="pre-new-release"
-release_regex="\b\.2\.rf$"
+release_regex="\b\.2$"
 
 if [ "$disttag" != "fc5" ] && echo $release | grep -q -E "$release_regex"; then
 	echo "** $script: Not building $app for $disttag/$arch since $release matches new release scheme."
